2d824f7fa9d49eb9e52e780a6d22eedbd651f00e
Hilda Amponsah <hilda.amp@hotmail.com>
2020-11-07 15:41:35 +0000

    girst commit- Arrays w each method

---
+++ Arrays/lib/element.rb
@@ -0,0 +1,5 @@
+class Elements
+  def print_now
+    2
+  end
+end
---
+++ Arrays/spec/element_spec.rb
@@ -0,0 +1,7 @@
+require 'element'
+
+describe Elements do
+   it "can print the element" do
+    expect(subject.print_now).to equal(2)
+   end
+end
